e0e34c13b691c4ce4b52c1003971ccab8eb56dba
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-08-29 19:48:20 +0500

    Added If not approval then cant login

--- app/controllers/storeins_controller.rb
+++ app/controllers/storeins_controller.rb
@@ -4,9 +4,17 @@ class StoreinsController < ApplicationController
   def create
   	store = Storeup.find_by_email(params[:email])
 
+  	@storing = Storeup.find_by_email(params[:email])
+
+  	
   	if store && store.authenticate(params[:password])
-  		session[:store_id] = store.id
-  		redirect_to root_url , :notice => "Store wala hai bhai"
+  		@approval = @storing.approval
+  		if @approval == "A"
+  			session[:store_id] = store.id
+  			redirect_to root_url , :notice => "Store wala hai bhai"
+  		end
+  	else
+  		redirect_to "/storelogin" , :notice => "Your store isnt approved !"
   	end
   end
   def destroy
